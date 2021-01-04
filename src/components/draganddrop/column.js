bdb52a88eba22227647ade9f0b25a6c8ae109af6
Aishwarya Shrestha <aishwaryashrestha4064@gmail.com>
2021-01-04 17:27:28 +0545

    Added

--- src/components/draganddrop/column.js
+++ src/components/draganddrop/column.js
@@ -6,61 +6,40 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
 
 export default class Column extends Component {
   
-    // state = {
-    //     anchorEl: null
-    // }
-
-
-    // handleClick = (e) => {
-    //     this.setState({ anchorEl: e.currentTarget });
-    // }
-
-    // handleClose = () => {
-    //     this.setState({ anchorEl: null });
-    // }
-
     state = {
-        open: true,
-        setOpen: true
+        anchorEl: null
+    }
+
+    handleClick = (e) => {
+        this.setState({ anchorEl: e.currentTarget });
+    }
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
     }
 
 
     render() {
       console.log(this.props.task);
         return (
-            <Grid item xs={4}>
+            <Grid item sm={12} md={4} lg={4}>
                 <Card elevation={0}>
                     <CardActions className={dragandropStyle.dragRemove}>
                         <div className={dragandropStyle.flexCenter}>
                             <div className={dragandropStyle.dragTitle}>
                                 {this.props.title}
                             </div>
-                            {/* <div className={dragandropStyle.menuIcon}>
-                                <i class="fas fa-plus"></i>
-                            </div> */}
-                            
-                            <IconButton className={dragandropStyle.addIcon}>
+                           
+                            <IconButton className={dragandropStyle.addIcon} aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick}>
                                 <i class="fas fa-plus"></i>
                             </IconButton>
                         </div>
-
-                        {/* <Button aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick}>
-                            Open Menu
-                        </Button> */}
-                        {/* <Menu
-                            id="simple-menu"
-                            anchorEl={this.state.anchorEl}
-                            keepMounted
-                            open={Boolean(this.state.anchorEl)}
-                            onClose={this.handleClose}
-                        >
-                            <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                            <MenuItem onClick={this.handleClose}>My account</MenuItem>
-                            <MenuItem onClick={this.handleClose}>Logout</MenuItem>
-                        </Menu> */}
                     </CardActions>
                     <CardContent 
                         className={dragandropStyle.cardBorder}
@@ -68,8 +47,34 @@ export default class Column extends Component {
                         onDrop={(event)=>{this.props.drop(event, this.props.type)}}
                     >
                     
-                        <Typography variant="body2" component="p">
-                        
+                    <Typography className={dragandropStyle.columnContainer}>
+                        <Menu
+                            id="simple-menu"
+                            getContentAnchorEl={null}
+                            anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'right',
+                            }}
+                            transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                            }}
+                            anchorEl={this.state.anchorEl}
+                            open={Boolean(this.state.anchorEl)}
+                            onClose={this.handleCose}
+                            className={dragandropStyle.newTaskField}
+                        >
+                            <TextareaAutosize aria-label="minimum height" rowsMin={5} placeholder="Enter a note" className={dragandropStyle.newTaskFieldForm}/>   
+                                <div className={dragandropStyle.btnFlex}>
+                                    <Button className={dragandropStyle.addButton}>
+                                        Add
+                                    </Button>
+                                    <Button className={dragandropStyle.cancelButton} onClick={this.handleClose}>
+                                        Cancel
+                                    </Button>
+                                </div>
+                        </Menu>
+        
                             {this.props.task.map(t=>{
                                
                                return <div className={dragandropStyle.cardRelative}>
@@ -77,14 +82,16 @@ export default class Column extends Component {
                                     onDragStart = {(event) => this.props.dragStart(event, t.taskName)}
                                     draggable
                                     className={dragandropStyle.draggable}>
-                                    {t.taskName}    
+                                    {t.taskName} 
                                 </div>
-                    
+
+                                <div  className={dragandropStyle.addedUser}>
+                                {t.add}   
+                                </div>
+
                                 <IconButton className={dragandropStyle.closeIcon}>
                                     <i class="fas fa-times"></i>
                                 </IconButton>
-
-                               
                                </div>
                             }  
                             )}
--- src/components/draganddrop/draganddrop.js
+++ src/components/draganddrop/draganddrop.js
@@ -3,15 +3,20 @@ import dragandropStyle from './draganddrop.module.scss';
 import Grid from '@material-ui/core/Grid';
 import Column from './column';
 
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
+
 export default class draganddrop extends Component {
     state = {
         tasks: [
-            {id: "1", taskName:"Implement Dashboard Layout", type:"toDo"},
-            {id: "2", taskName:"Add button to card header", type: "toDo"},
-            {id: "3", taskName:"Allow users to remove tasks", type: "toDo"},
-            {id: "4", taskName:"Allow users to add tasks", type:"inProgress"},
-            {id: "5", taskName: "Implement Grid Card Layout", type: "Done"},
-            {id: "6", taskName: "Drag and Drop Tasks", type: "Done"}
+            {id: "1", taskName:"Implement Dashboard Layout", type:"toDo", add:"Added by aishustha"},
+            {id: "2", taskName:"Add button to card header", type: "toDo", add:"Added by aishustha"},
+            {id: "3", taskName:"Allow users to remove tasks", type: "toDo", add:"Added by aishustha"},
+            {id: "4", taskName:"Allow users to add tasks", type:"inProgress", add:"Added by aishustha"},
+            {id: "5", taskName: "Implement Grid Card Layout", type: "Done", add:"Added by aishustha"},
+            {id: "6", taskName: "Drag and Drop Tasks", type: "Done", add:"Added by aishustha"}
         ],
 
     }
@@ -55,7 +60,7 @@ export default class draganddrop extends Component {
         return (
             <div className={dragandropStyle.dragContainer}>
                 <h2>Drag & Drop</h2>
-                <Grid container className={dragandropStyle.dragFlex} spacing={5}>
+                <Grid container className={dragandropStyle.dragFlex} spacing={3}>
 
                     <Column 
                     dragOver={this.onDragOver} 
@@ -64,7 +69,10 @@ export default class draganddrop extends Component {
                     type="toDo"
                     dragStart={this.onDragStart}
 
-                    />
+                    >
+                    
+                    </Column>
+
                     <Column 
                         dragOver={this.onDragOver} 
                         drop={this.onDrop} 
